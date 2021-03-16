a281fe116ea9b145eb1f20f50cef5722f0fe7f62
Yevhen Chemes <chemes@gmail.com>
2021-03-16 18:17:35 +0200

    feat: First release version

---
+++ bot.sh
@@ -0,0 +1,31 @@
+set -e
+option="${1}"
+default_version='0.0.0-SNAPSHOT'
+next_version="${2:-$default_version}"
+case ${option} in
+   --prepare)
+      ./mvnw \
+        -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -V \
+        clean install -Drevision="$next_version"
+      ;;
+   --publish)
+      # Publish Docker in Github Packages
+      DOCKER_PKG=docker.pkg.github.com/bhuwanupadhyay/semantic-versioning-on-docker-build-and-helm-chart/my-service:"$next_version"
+      docker login docker.pkg.github.com -u BhuwanUpadhyay -p "$GITHUB_TOKEN"
+      docker tag docker.io/bhuwanupadhyay/my-service:"$next_version" "$DOCKER_PKG"
+      docker push "$DOCKER_PKG"
+
+      # Publish Helm chart in Github Releases
+      HELM_CHART="my-service-$next_version.tgz"
+      HELM_CHART_FILE_PATH="$(pwd)/target/helm/repo/$FILE_NAME"
+
+      # TODO: yourself
+      # Write a suitable script to upload your helm chart in your Artifactory or chart museum.
+
+      echo "Mock publish: $HELM_CHART from $HELM_CHART_FILE_PATH"
+      ;;
+   *)
+      echo "`basename ${0}`:usage: [--prepare] | [--publish]"
+      exit 1 # Command to come out of the program with status 1
+      ;;
+esac
---
+++ src/main/kotlin/io/github/bhuwanupadhyay/example/NameManager.kt
@@ -0,0 +1,11 @@
+@Configuration
+class NameRoutes(private val handler: NameHandler) {
+
+    @Bean
+    fun router() = router {
+        accept(APPLICATION_JSON).nest {
+            GET("/names/{given-name}", handler::findGivenName)
+        }
+    }
+
+}
